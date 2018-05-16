ed6eb30842a1d82cb949dea557a0db1991d610ce
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-16 08:51:57 -0400

    Reformatted a nested hash

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  updated_hash = {}
+  languages.each do |key,value|
+    value.each do |language, data_hash|
+      updated_hash[language] = data_hash
+        if language == :javascript
+          updated_hash[language][:style] = [:oo, :functional]
+        else
+          updated_hash[language][:style] = [key]
+        end
+     end
+  end
+  updated_hash
 end
